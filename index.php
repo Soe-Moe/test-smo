29bec9d64f3e632d1dab66165d9b6aac9656567c
Soe Moe <soemoeoo1111@gmail.com>
2020-11-10 11:52:43 +0630

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    Hello world
-</body>
-
-</html>
+echo 'Hello World';
